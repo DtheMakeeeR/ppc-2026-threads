734cfda6da07312d06f3e431c7abf21e1217df53
root <the_h1nk@mail.ru>
2026-03-30 14:35:08 +0000

    test

--- tasks/zavyalov_a_complex_sparse_matrix_mult/tests/functional/main.cpp
+++ tasks/zavyalov_a_complex_sparse_matrix_mult/tests/functional/main.cpp
@@ -12,6 +12,7 @@
 #include "util/include/util.hpp"
 #include "zavyalov_a_complex_sparse_matrix_mult/common/include/common.hpp"
 #include "zavyalov_a_complex_sparse_matrix_mult/seq/include/ops_seq.hpp"
+#include "zavyalov_a_complex_sparse_matrix_mult/tbb/include/ops_tbb.hpp"
 
 namespace zavyalov_a_compl_sparse_matr_mult {
 
@@ -141,6 +142,8 @@ const std::array<TestType, 10> kTestParam = {
     std::make_tuple(1, 5, 3), std::make_tuple(4, 7, 2)};
 
 const auto kTestTasksList = std::tuple_cat(ppc::util::AddFuncTask<ZavyalovAComplSparseMatrMultSEQ, InType>(
+    kTestParam, PPC_SETTINGS_zavyalov_a_complex_sparse_matrix_mult),
+  ppc::util::AddFuncTask<ZavyalovAComplSparseMatrMultTBB, InType>(
     kTestParam, PPC_SETTINGS_zavyalov_a_complex_sparse_matrix_mult));
 
 const auto kGtestValues = ppc::util::ExpandToValues(kTestTasksList);

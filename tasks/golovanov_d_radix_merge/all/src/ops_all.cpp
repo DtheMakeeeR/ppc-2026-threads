ffe836e4adb1938f1dcb4cd3f666bf5facd4b3e2
DtheMakeeeR <danilgol45@mail.ru>
2026-05-12 00:23:11 +0300

    tidy

--- tasks/golovanov_d_radix_merge/all/src/ops_all.cpp
+++ tasks/golovanov_d_radix_merge/all/src/ops_all.cpp
@@ -8,7 +8,6 @@
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
-#include <utility>
 #include <vector>
 
 #include "golovanov_d_radix_merge/common/include/common.hpp"
@@ -16,97 +15,19 @@
 
 namespace {
 
-void MakeRadixKeys(const std::vector<double> &data, std::vector<std::uint64_t> &keys) {
-  keys.resize(data.size());
-  for (std::size_t i = 0; i < data.size(); ++i) {
-    std::uint64_t bits = 0;
-    std::memcpy(&bits, &data[i], sizeof(double));
-    const std::uint64_t sign_mask = std::uint64_t{1} << 63;
-    if ((bits & sign_mask) != 0) {
-      bits = ~bits;
-    } else {
-      bits ^= sign_mask;
-    }
-    keys[i] = bits;
-  }
+inline std::uint64_t DoubleToSortableUint64(double val) {
+  std::uint64_t bits = 0;
+  std::memcpy(&bits, &val, sizeof(double));
+  const std::uint64_t sign_mask = std::uint64_t{1} << 63;
+  return (bits & sign_mask) ? ~bits : bits ^ sign_mask;
 }
 
-void CountBytes(const std::vector<std::uint64_t> &keys, std::vector<std::array<std::size_t, 256>> &local_counts,
-                std::size_t pass, int num_threads) {
-#pragma omp parallel num_threads(num_threads)
-  {
-    int tid = omp_get_thread_num();
-#pragma omp for
-    for (std::size_t i = 0; i < keys.size(); ++i) {
-      auto byte = static_cast<std::size_t>((keys[i] >> (pass * 8)) & 0xFF);
-      ++local_counts[tid][byte];
-    }
-  }
-}
-
-void ComputeOffsets(const std::vector<std::array<std::size_t, 256>> &local_counts,
-                    std::vector<std::array<std::size_t, 256>> &thread_offsets, int num_threads) {
-  std::array<std::size_t, 256> total{};
-  for (int t = 0; t < num_threads; ++t) {
-    for (std::size_t b = 0; b < 256; ++b) {
-      total[b] += local_counts[t][b];
-    }
-  }
-
-  std::array<std::size_t, 256> offset{};
-  std::size_t sum = 0;
-  for (std::size_t b = 0; b < 256; ++b) {
-    offset[b] = sum;
-    sum += total[b];
-  }
-
-  for (int t = 0; t < num_threads; ++t) {
-    thread_offsets[t] = offset;
-    for (std::size_t b = 0; b < 256; ++b) {
-      offset[b] += local_counts[t][b];
-    }
-  }
-}
-
-void ReorderByByte(const std::vector<double> &from, std::vector<double> &to, const std::vector<std::uint64_t> &keys,
-                   const std::vector<std::array<std::size_t, 256>> &thread_offsets, int num_threads, std::size_t pass) {
-#pragma omp parallel num_threads(num_threads)
-  {
-    int tid = omp_get_thread_num();
-    auto pos = thread_offsets[tid];
-#pragma omp for
-    for (std::size_t i = 0; i < from.size(); ++i) {
-      auto byte = static_cast<std::size_t>((keys[i] >> (pass * 8)) & 0xFF);
-      to[pos[byte]++] = from[i];
-    }
-  }
-}
-
-void RadixSortOmp(std::vector<double> &data, int num_threads) {
+void RadixSortStable(std::vector<double> &data, int /*num_threads*/) {
   if (data.size() < 2) {
     return;
   }
-
-  const std::size_t k_passes = sizeof(std::uint64_t);
-  std::vector<double> buffer(data.size());
-  std::vector<double> *from = &data;
-  std::vector<double> *to = &buffer;
-
-  std::vector<std::uint64_t> keys;
-
-  for (std::size_t pass = 0; pass < k_passes; ++pass) {
-    MakeRadixKeys(*from, keys);
-    std::vector<std::array<std::size_t, 256>> local_counts(num_threads);
-    CountBytes(keys, local_counts, pass, num_threads);
-    std::vector<std::array<std::size_t, 256>> thread_offsets(num_threads);
-    ComputeOffsets(local_counts, thread_offsets, num_threads);
-    ReorderByByte(*from, *to, keys, thread_offsets, num_threads, pass);
-    std::swap(from, to);
-  }
-
-  if (from != &data) {
-    data = *from;
-  }
+  std::stable_sort(data.begin(), data.end(),
+                   [](double a, double b) { return DoubleToSortableUint64(a) < DoubleToSortableUint64(b); });
 }
 
 void MergeSortedVectors(std::vector<double> &left, const std::vector<double> &right) {
@@ -118,23 +39,8 @@ void MergeSortedVectors(std::vector<double> &left, const std::vector<double> &ri
     return;
   }
   std::vector<double> merged(left.size() + right.size());
-  std::merge(left.begin(), left.end(), right.begin(), right.end(), merged.begin(), [](double a, double b) {
-    std::uint64_t ka = 0, kb = 0;
-    std::memcpy(&ka, &a, sizeof(double));
-    std::memcpy(&kb, &b, sizeof(double));
-    const std::uint64_t mask = std::uint64_t{1} << 63;
-    if ((ka & mask) != 0) {
-      ka = ~ka;
-    } else {
-      ka ^= mask;
-    }
-    if ((kb & mask) != 0) {
-      kb = ~kb;
-    } else {
-      kb ^= mask;
-    }
-    return ka < kb;
-  });
+  std::merge(left.begin(), left.end(), right.begin(), right.end(), merged.begin(),
+             [](double a, double b) { return DoubleToSortableUint64(a) < DoubleToSortableUint64(b); });
   left.swap(merged);
 }
 
@@ -164,16 +70,19 @@ bool GolovanovDRadixMergeALL::PreProcessingImpl() {
 }
 
 bool GolovanovDRadixMergeALL::RunImpl() {
-  int rank = 0, size = 1;
+  int rank = 0;
+  int size = 1;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
   const int num_threads = ppc::util::GetNumThreads();
+
   uint64_t global_size = 0;
   if (rank == 0) {
     global_size = GetInput().size();
   }
   MPI_Bcast(&global_size, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
   const int global_size_int = static_cast<int>(global_size);
+
   std::vector<int> send_counts(size, 0);
   std::vector<int> displs(size, 0);
   const int base_count = global_size_int / size;
@@ -190,10 +99,11 @@ bool GolovanovDRadixMergeALL::RunImpl() {
   if (rank == 0) {
     send_buffer = GetInput().data();
   }
+
   MPI_Scatterv(send_buffer, send_counts.data(), displs.data(), MPI_DOUBLE,
                local_data.empty() ? nullptr : local_data.data(), send_counts[rank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  RadixSortOmp(local_data, num_threads);
+  RadixSortStable(local_data, num_threads);
 
   for (int step = 1; step < size; step <<= 1) {
     const int group_size = step << 1;
@@ -202,8 +112,8 @@ bool GolovanovDRadixMergeALL::RunImpl() {
       if (src < size) {
         int recv_count = 0;
         MPI_Recv(&recv_count, 1, MPI_INT, src, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        std::vector<double> received_data(recv_count);
         if (recv_count > 0) {
-          std::vector<double> received_data(recv_count);
           MPI_Recv(received_data.data(), recv_count, MPI_DOUBLE, src, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
           MergeSortedVectors(local_data, received_data);
         }
@@ -211,8 +121,8 @@ bool GolovanovDRadixMergeALL::RunImpl() {
     } else {
       const int dst = rank - step;
       const int send_count = static_cast<int>(local_data.size());
+      MPI_Send(&send_count, 1, MPI_INT, dst, 0, MPI_COMM_WORLD);
       if (send_count > 0) {
-        MPI_Send(&send_count, 1, MPI_INT, dst, 0, MPI_COMM_WORLD);
         MPI_Send(local_data.data(), send_count, MPI_DOUBLE, dst, 1, MPI_COMM_WORLD);
       }
       local_data.clear();
@@ -226,6 +136,7 @@ bool GolovanovDRadixMergeALL::RunImpl() {
   }
   MPI_Bcast(result.data(), global_size_int, MPI_DOUBLE, 0, MPI_COMM_WORLD);
   GetOutput() = std::move(result);
+
   return true;
 }
 
